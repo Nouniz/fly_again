9ef2292dcd77defb20c7367d02dfdce3c9ab46b2
Deniz OZDEMIR <deniz@MacBook-Air-de-Deniz.local>
2015-02-18 08:46:45 +0100

    first commit

---
+++ Assets/Scripts/Obstables.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstables : MonoBehaviour {
+
+	public Vector2 velocity = new Vector2(-4, 0);
+	
+	// Use this for initialization
+	void Start()
+	{
+		rigidbody2D.velocity = velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public Vector2 jumpForce = new Vector2(0, 300);
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// saut
+		if (Input.GetKeyUp("space"))
+		{
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForce);
+		}
+	}
+}
